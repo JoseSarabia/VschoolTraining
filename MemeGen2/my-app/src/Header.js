d8f3e803124e4f618649a697ca71747a1b61b1c3
Jose Sarabia <jjsara92@gmail.com>
2022-03-09 19:59:18 -0500

    initial commit

---
+++ MemeGen2/my-app/src/Header.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img src="https://images.pexels.com/photos/6973960/pexels-photo-6973960.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+            className="headerImage"
+            />
+            <h2 className="headerName">The Meme Producer</h2>
+            <h4 className="headerProject">meme Genarator</h4>
+        </header>
+    )
+}
